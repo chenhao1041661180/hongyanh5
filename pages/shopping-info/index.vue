8f61140059a060800c3e93454429148a708b84a6
rocky <1041661180@qq.com>
2022-03-28 09:04:40 +0800

    购物车跳转

--- pages/shopping-info/index.vue
+++ pages/shopping-info/index.vue
@@ -1,178 +1,231 @@
 <template>
   <!-- 商品详情 -->
   <view style="display: flex;flex-direction: column;position: relative;height: 100%;width: 750rpx;">
-    <u-navbar back-icon-color="#FFFFFF" :background="{background: 'rgba(255,255,255,0)'}" z-index="333"
-      :border-bottom="false" />
-    <u-swiper :list="bannerList" indicator-pos="bottomCenter" border-radius="0" height="750"
-      style="position: absolute;top: 0;left: 0;right: 0;" />
+    <u-navbar
+      :background="{background: 'rgba(255,255,255,0)'}"
+      :border-bottom="false"
+      back-icon-color="#FFFFFF"
+      z-index="333"
+    />
+    <u-swiper
+      :list="bannerList"
+      indicator-pos="bottomCenter"
+      border-radius="0"
+      height="750"
+      style="position: absolute;top: 0;left: 0;right: 0;"
+    />
     <view class="sp-view">
-      <text class="title-text">{{goodsDetail.goodsName}}</text>
+      <text class="title-text">{{ goodsDetail.goodsName }}</text>
 
       <view class="jg-view">
-        <text class="price-text">￥{{goodsDetail.goodsPrice}}</text>
-        <text class="xl-text">销量 {{goodsDetail.goodsSales}}</text>
-        <text class="kc-text">库存 {{goodsDetail.goodsInventory}}</text>
-        <text class="qdl-text">起订量 {{goodsDetail.goodsMoq}}</text>
+        <text class="price-text">￥{{ goodsDetail.goodsPrice }}</text>
+        <text class="xl-text">销量 {{ goodsDetail.goodsSales }}</text>
+        <text class="kc-text">库存 {{ goodsDetail.goodsInventory }}</text>
+        <text class="qdl-text">起订量 {{ goodsDetail.goodsMoq }}</text>
       </view>
-
 
     </view>
-    <view class="sp-view" style="margin-top:10rpx ;align-items: center;display: flex;margin-bottom: 114rpx;">
+    <view
+      class="sp-view"
+      style="margin-top:10rpx ;align-items: center;display: flex;margin-bottom: 114rpx;"
+    >
 
       <view style="align-items: center;">
-        <image src="../../static/images/img_shangpinxiangqing.png" style="width: 26rpx;height: 26rpx;" />
+        <image
+          src="../../static/images/img_shangpinxiangqing.png"
+          style="width: 26rpx;height: 26rpx;"
+        />
         <text
-          style="color: #666666 ; font-size: 32rpx;font-family: PingFangSC-Regular; margin-left: 10rpx;margin-right: 10rpx;">商品详情</text>
-        <image src="../../static/images/img_shangpinxiangqing.png" style="width: 26rpx;height: 26rpx;" />
+          style="color: #666666 ; font-size: 32rpx;font-family: PingFangSC-Regular; margin-left: 10rpx;margin-right: 10rpx;"
+        >商品详情</text>
+        <image
+          src="../../static/images/img_shangpinxiangqing.png"
+          style="width: 26rpx;height: 26rpx;"
+        />
       </view>
-      <text class="goods-describe-text">{{goodsDetail.goodsDescribe}}</text>
+      <text class="goods-describe-text">{{ goodsDetail.goodsDescribe }}</text>
       <view style="flex-direction: column;display: flex;width: 100%;height: auto;">
-        <u-lazy-load threshold="-100" border-radius="0" :image="detailPictureUrl(item)" :index="index" imgMode="widthFix"
-          v-for="(item,index) in goodsDetail.detailPicture" />
+        <u-lazy-load
+          v-for="(item,index) in goodsDetail.detailPicture"
+          :image="detailPictureUrl(item)"
+          :index="index"
+          threshold="-100"
+          border-radius="0"
+          img-mode="widthFix"
+        />
 
         <!-- <image mode="widthFix" :src="detailPictureUrl(item)" style="width:100%;margin-top: 12rpx;"
           v-for="(item,index) in goodsDetail.detailPicture" /> -->
       </view>
 
-
     </view>
 
     <view class="bottom-view">
       <view style="display: flex;flex: 1;justify-content: space-around;">
-        <view class="bottom-item-view" @click="toHome">
-          <u-icon custom-prefix="hongyan-icon" name="a-ic_shop2x" size="36" color="#333333"></u-icon>
+        <view
+          class="bottom-item-view"
+          @click="toHome"
+        >
+          <u-icon
+            custom-prefix="hongyan-icon"
+            name="a-ic_shop2x"
+            size="36"
+            color="#333333"
+          />
           <text class="item-text">店铺</text>
         </view>
-        <view class="bottom-item-view">
-          <u-icon custom-prefix="hongyan-icon" name="a-ic_shopping_cart2x" size="36" color="#333333"></u-icon>
+        <view
+          class="bottom-item-view"
+          @click="toGwc"
+        >
+          <u-icon
+            custom-prefix="hongyan-icon"
+            name="a-ic_shopping_cart2x"
+            size="36"
+            color="#333333"
+          />
           <text class="item-text">购物车</text>
-          <text class="gwc-num" v-show="cartCount!=0">{{cartCount}}</text>
+          <text
+            v-show="cartCount!=0"
+            class="gwc-num"
+          >{{ cartCount }}</text>
         </view>
       </view>
-      <text class="bottom-btn-text" @click="addCart">加入购物车</text>
-      <text class="bottom-btn-text gm-text" @click="toConfirmOrder">立即购买</text>
+      <text
+        class="bottom-btn-text"
+        @click="addCart"
+      >加入购物车</text>
+      <text
+        class="bottom-btn-text gm-text"
+        @click="toConfirmOrder"
+      >立即购买</text>
     </view>
   </view>
 </template>
 
 <script>
-  import {
-    goodsInfo
-  } from '@/api/home.js'
-  import {
-    getCartCount,
-    addCart
-  } from '@/api/shopp-cart.js'
-  export default {
-    data() {
-      return {
+import {
+  goodsInfo
+} from '@/api/home.js'
+import {
+  getCartCount,
+  addCart
+} from '@/api/shopp-cart.js'
+export default {
+  data() {
+    return {
 
-        id: '',
-        cartCount: 0, //购物车总数
-        goodsDetail: {},
-        bannerList: []
-
-      }
-    },
-    onLoad(option) {
-      this.id = option.id
-    },
-
-    mounted() {
-
-      this.goodsInfo()
-      this.getCartCount()
-    },
-    computed: {
-      detailPictureUrl() {
-        return function(item) {
-          let imageUrl = uni.$util.assetsPath.IMAGE_URL + item.pictureUrl;
-          console.log(imageUrl)
-          return imageUrl
-        }
-
-      }
-    },
-    methods: {
-      toConfirmOrder() {
-
-        // let idArr = []
-        // this.cartList.forEach(item => {
-        //   if (item.select) {
-        //     idArr.push(item.id)
-        //   }
-        // })
-
-        uni.navigateTo({
-          url: `./confirm-order?&orderWay=1&ids=${this.goodsDetail.id}`
-        })
-      },
-      /**
-       * 商品详情
-       */
-      goodsInfo() {
-        goodsInfo(this.id)
-          .then(res => {
-            this.goodsDetail = res.data
-            // this.goodsDetail.detailPicture.push({
-            //   pictureUrl: 'https://cdn.uviewui.com/uview/swiper/1.jpg'
-            // })
-
-            this.goodsDetail.banner.forEach((item) => {
-              this.bannerList.push(uni.$util.assetsPath.IMAGE_URL + item.pictureUrl)
-            })
-          })
-          .catch(err => {
-
-          })
-      },
-      /**
-       * 购物车总数
-       */
-      getCartCount() {
-        getCartCount()
-          .then(res => {
-            let count = res.data
-            if (count > 99) {
-              this.cartCount = "99+"
-            } else {
-              this.cartCount = count
-            }
-
-          })
-          .catch(err => {
-
-          })
-      },
-      /**
-       * 添加到购物车
-       */
-      addCart() {
-        uni.showLoading({
-          title: ''
-        })
-        addCart(this.goodsDetail.id)
-          .then(res => {
-            uni.hideLoading()
-            uni.showToast({
-              icon: 'none',
-              title: "添加成功"
-            })
-            this.getCartCount()
-          }).catch(err => {
-            uni.hideLoading()
-          })
-
-      },
-      toHome() {
-        let enstring =  uni.$util.uniStore.getStorage("enstring");
-         uni.reLaunch({
-           url: `../home/index?enstring=${enstring}`
-         })
-      }
+      id: '',
+      cartCount: 0, // 购物车总数
+      goodsDetail: {},
+      bannerList: []
 
     }
+  },
+  onLoad(option) {
+    this.id = option.id
+  },
+  computed: {
+    detailPictureUrl() {
+      return function(item) {
+        const imageUrl = uni.$util.assetsPath.IMAGE_URL + item.pictureUrl
+        console.log(imageUrl)
+        return imageUrl
+      }
+    }
+  },
+
+  mounted() {
+    this.goodsInfo()
+    this.getCartCount()
+  },
+  methods: {
+    toConfirmOrder() {
+      // let idArr = []
+      // this.cartList.forEach(item => {
+      //   if (item.select) {
+      //     idArr.push(item.id)
+      //   }
+      // })
+
+      uni.navigateTo({
+        url: `./confirm-order?&orderWay=1&ids=${this.goodsDetail.id}`
+      })
+    },
+    /**
+       * 商品详情
+       */
+    goodsInfo() {
+      goodsInfo(this.id)
+        .then(res => {
+          this.goodsDetail = res.data
+          // this.goodsDetail.detailPicture.push({
+          //   pictureUrl: 'https://cdn.uviewui.com/uview/swiper/1.jpg'
+          // })
+
+          this.goodsDetail.banner.forEach((item) => {
+            this.bannerList.push(uni.$util.assetsPath.IMAGE_URL + item.pictureUrl)
+          })
+        })
+        .catch(err => {
+
+        })
+    },
+    /**
+       * 购物车总数
+       */
+    getCartCount() {
+      getCartCount()
+        .then(res => {
+          const count = res.data
+          if (count > 99) {
+            this.cartCount = '99+'
+          } else {
+            this.cartCount = count
+          }
+        })
+        .catch(err => {
+
+        })
+    },
+    /**
+       * 添加到购物车
+       */
+    addCart() {
+      uni.showLoading({
+        title: ''
+      })
+      addCart(this.goodsDetail.id)
+        .then(res => {
+          uni.hideLoading()
+          uni.showToast({
+            icon: 'none',
+            title: '添加成功'
+          })
+          this.getCartCount()
+        }).catch(err => {
+          uni.hideLoading()
+        })
+    },
+    toHome() {
+      const enstring = uni.$util.uniStore.getStorage('enstring')
+      uni.reLaunch({
+        url: `../home/index?enstring=${enstring}`
+      })
+    },
+    // 购物车
+    toGwc() {
+      uni.$emit('shopping-cart')
+      setTimeout(()=>{
+        uni.switchTab({
+          url: `../shopping-cart/index`
+        })
+      },50)
+
+    }
+
   }
+}
 </script>
 
 <style scoped>
